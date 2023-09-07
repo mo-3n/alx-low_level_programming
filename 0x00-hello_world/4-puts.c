42759614e280385713e958bc0e96157ec609459c
mo-3n <mouad3n@gmail.com>
2023-09-07 23:44:09 +0000

    Add 0x00-hello_world folder

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * This C file is using puts function
+ */
+int main()
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * This C source file is using printf function
+ */
+int main()
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return 0;
+}
