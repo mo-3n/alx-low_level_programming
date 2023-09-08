d14403dc68016d124a882e6460e45908aee078d8
mo-3n <mouad3n@gmail.com>
2023-09-08 00:42:55 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 /**
- * This C file is using puts function
+ * This is a simple C source file
+ */
+
+/**
+ * This fuction prints a string
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/* return 0 as a success code */
 	return (0);
 }
