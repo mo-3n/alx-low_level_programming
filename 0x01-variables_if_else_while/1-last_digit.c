944dc60a3dc7b957d7784d08eeae5b61cd65dbd0
mo-3n <mouad3n@gmail.com>
2023-09-09 21:23:17 +0000

    Add 0x01 variable if else while Folder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int n;
 	int lastdigit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	scanf("%d", &n);
@@ -21,8 +22,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	}
 	else if (lastdigit == 0)
-	{
-		
+	{		
 		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 	}
 	else
