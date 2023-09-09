0e5933b700e776260c8c973a6fd27067d0caff29
mo-3n <mouad3n@gmail.com>
2023-09-09 22:55:00 +0000

    Add 0x01 variable if else while Folder

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 	for (i = 0; i < 9; i++)
 	{
 		putchar('0' + i);
-		putchar(', ');
-		/*putchar(' ');*/
+		putchar(',');
+		putchar(' ');
 	}
 	for (; i == 9; i++)
 	{
-		putchar('0' + i);
+		putchar('0' + i\n);
 	}
-	putchar('\n');
+	/*putchar('\n');*/
 	return (0);
 }
