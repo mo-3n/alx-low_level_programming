8c95ad8ca139856c322c2ec2bdab6c92dcb5f7b1
mo-3n <mouad3n@gmail.com>
2023-09-09 03:29:56 +0000

    Add 0x01 variable if else while Folder

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0, n = 0, z = 0, d = 1; i < 10 && n < 9 && z < 10 && d < 10; i++, n++, z++, d++)
+	{
+		putchar(i);
+		putchar(n);
+		putchar(' ');
+		putchar(z);
+		putchar(d);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
