8f64df3de7d7a2b25d59d3b8720bec0a01ef73ef
mo-3n <mouad3n@gmail.com>
2023-09-09 02:50:56 +0000

    Add 0x01 variable if else while Folder

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbr = 0;
+	while (numbr < 10)
+	{
+		printf("%d", numbr);
+	}
+	printf("\n");
+	return (0);
+}
