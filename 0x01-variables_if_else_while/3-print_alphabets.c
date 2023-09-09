06b293178de5367b72a3fb61d297a09b169bb318
mo-3n <mouad3n@gmail.com>
2023-09-09 02:35:52 +0000

    Add 0x01 variable if else while Folder

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az[26] = "abcdefghijklmnopqrstuvwxyz";
+	char AZ[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(az[i]);
+		putchar(AZ[i]);
+	}
+	putchar('\n');
+	return (0);
+}
