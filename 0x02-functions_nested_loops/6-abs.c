50ba6a5030ceaa8c6638e20b495d78bc0b8d04b3
mo-3n <mouad3n@gmail.com>
2023-09-14 01:57:44 +0000

    Add 0x02 Functions Nested Loops Folder

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * This file is about _abs function
+ */
+
+int _abs(int)
+{
+	int absvalue;
+	
+	if (n >= 0)
+	{
+		absvalue = n;
+		return (absvalue);
+	}
+	else
+	{
+		absvalue = n * -1;
+		return (absvalue);
+	}
+}
