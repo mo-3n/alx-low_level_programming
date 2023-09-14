72b9626ebacf4c00abe5ecd18c9de502598657ec
mo-3n <mouad3n@gmail.com>
2023-09-14 01:46:31 +0000

    Add 0x02 Functions Nested Loops Folder

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * This file is about print_sign function
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
