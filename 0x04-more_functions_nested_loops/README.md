1c958c0670882d1f02822d6f6b735d22338d937b
mo-3n <mouad3n@gmail.com>
2023-09-15 23:11:03 +0000

    Add 0x04 More Functions Nested Loops Folder

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is 0x04 More Functions Nested Loops Folder
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+/*
+{
+	return write(1, &c, 1);
+}
+*/
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
