446db233e791eb893b8a81330b9b6a9b60eeeded
mo-3n <mouad3n@gmail.com>
2023-09-15 23:56:21 +0000

    Add 0x04 More Functions Nested Loops Folder

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * This file is about print_numbers function
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -18,4 +18,25 @@ int _isdigit(int c);
 /* Function prototype for mul function */
 int mul(int a, int b);
 
+/* Function prototype for print_numbers function */
+void print_numbers(void);
+
+
+void print_most_numbers(void);
+
+
+void more_numbers(void);
+
+
+void print_line(int n);
+
+
+void print_diagonal(int n);
+
+
+void print_square(int size);
+
+
+void print_triangle(int size);
+
 #endif /* MAIN_H */
