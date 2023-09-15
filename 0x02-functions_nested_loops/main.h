088dc9bc1b02f759bcbbceb1b5ec0f4177fb480f
mo-3n <mouad3n@gmail.com>
2023-09-15 19:12:15 +0000

    Add 0x02 Functions Nested Loops Folder

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /* Fuction prototype for _putchar function */
-int _putchar(char c)
+/*int _putchar(char c)
 {
 	return write(1, &c, 1);
-};
+	};*/
 
 /* Function prototype for _islower function */
 int _islower(int c);
