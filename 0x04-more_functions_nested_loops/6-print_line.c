9cf1922b3d0197afac508c3e85b9c30af3b77758
mo-3n <mouad3n@gmail.com>
2023-09-16 02:04:38 +0000

    Add 0x04 More Functions Nested Loops Folder

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * This file is about print_line function
+ */
+
+void print_line(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		int j;
+		
+		for (j = 0; j < n; j++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
+}
