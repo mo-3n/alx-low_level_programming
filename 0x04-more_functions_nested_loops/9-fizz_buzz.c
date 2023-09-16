9e6d576ba1adebe6f8fc138ab6c8034cc08a29fb
mo-3n <mouad3n@gmail.com>
2023-09-16 03:11:51 +0000

    Add 0x04 More Functions Nested Loops Folder

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * This file is about fizz_buzz test
+ */
+int main(int i)
+{
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		if else (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		if else (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		printf("\n");
+	}
+	printf('\n');
+}
