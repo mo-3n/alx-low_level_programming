1fc9b2591f8fb9ab4a4faa1016ce00f2e3207dc8
mo-3n <mouad3n@gmail.com>
2023-09-16 02:28:39 +0000

    Add 0x04 More Functions Nested Loops Folder

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * This file is about print_diagonal function
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+				_putchar('\\');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
