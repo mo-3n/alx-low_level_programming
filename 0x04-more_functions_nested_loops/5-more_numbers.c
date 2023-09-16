ff2e1c222407939b52b0b9b67433526c0411b8d3
mo-3n <mouad3n@gmail.com>
2023-09-16 01:40:44 +0000

    Add 0x04 More Functions Nested Loops Folder

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,6 @@
 
 void more_numbers(void)
 {
-/*
-	int j;
-	
-	for (j = 0; j < 10; j++)
-	{
-		int i;
-		
-		for (i = 0; i < 16; i++)
-		{
-			if 
-			_putchar(i + '0');
-		}
-		_putchar('\n');
-	}
-*/
-
 	char seq[20] = "01234567891011121314";
 	int i;
 
