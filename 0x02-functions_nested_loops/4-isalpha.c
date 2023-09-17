4eaa71a9ac3aad40e3d5cbdc165ae0298c5d1bb4
mo-3n <mouad3n@gmail.com>
2023-09-17 20:35:16 +0000

    Add 0x02 Functions Nested Loops Folder

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * This file is about _isalpha function
+ * _isalpha - This function do something.
+ *
+ * @c: This function perform an action,
+ *
+ * Return: end of description.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
