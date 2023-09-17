8b4d4dc163cc50bfd4aad494fed3d848ed2acf4f
mo-3n <mouad3n@gmail.com>
2023-09-17 19:55:54 +0000

    Add 0x02 Functions Nested Loops Folder

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,26 @@ int _putchar(char c);
 /* Function prototype for _islower function */
 int _islower(int c);
 
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
 #endif /* MAIN.H */
