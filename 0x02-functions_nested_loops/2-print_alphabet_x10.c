dbd1cb25daca2cc4628aa771eae6a3f2c28409ec
mo-3n <mouad3n@gmail.com>
2023-09-17 20:06:58 +0000

    Add 0x02 Functions Nested Loops Folder

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 void print_alphabet_x10(void)
 {
 	char az[26] = "abcdefghijklmnopqrstuvwxyz";
-	int j = 0 , i;
+	int j = 0, i;
 
 	while (j < 10)
 	{
 		i = 0;
-		
-		while(i < 26)
+
+		while (i < 26)
 		{
 		_putchar(az[i]);
 		i++;
