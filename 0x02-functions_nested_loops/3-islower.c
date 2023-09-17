4171ad559d1db8c97dfee88fa7bf305a1ae1a995
mo-3n <mouad3n@gmail.com>
2023-09-17 20:33:03 +0000

    Add 0x02 Functions Nested Loops Folder

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,9 @@
 /**
  * _islower - This function do something.
  *
- * This function take an int c and perform an action,
- * and return 1 or 0.
+ * @c: This function perform an action,
+ *
+ * Return: end of description.
  */
 int _islower(int c)
 {
