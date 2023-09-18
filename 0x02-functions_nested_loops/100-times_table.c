b47e5cdf280ebfd5aa03a833bee1d122e8c7338b
mo-3n <mouad3n@gmail.com>
2023-09-18 03:32:20 +0000

    Add 0x02 Functions Nested Loops Folder

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * times_table - This function do something.
+ *
+ * Argument description.
+ * Return value description.
+ */
+void print_times_table(int n)
+{
+	int x = 0;
+	int y = 0;
+	int m;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			if (y > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			_putchar('0' + x * y);
+		}
+		_putchar('\n');
+
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				m = x * y;
+
+				if (m >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + m / 10);
+					_putchar('0' + m % 10);
+				}
+				else
+				{
+					if (m > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + m);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
