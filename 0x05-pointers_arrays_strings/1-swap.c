66c69e9d76ce1bcb00c9d28921a78bddb8fe9145
mo-3n <mouad3n@gmail.com>
2023-09-18 22:59:26 +0000

    Add 0x05 Pointers Arrays Strings Folder

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - This function do something.
+ *
+ * @a: Argument description.
+ *
+ * Return value description.
+ */
+void swap_int(int *a, int *b)
+{
+	int tempvar;
+
+	tempvar = a;
+	*a = b;
+	*b = tempvar;
+}
