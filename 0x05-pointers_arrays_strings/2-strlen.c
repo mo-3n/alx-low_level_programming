5b89d029b7bd130b09130aa97aaa157eb168d865
mo-3n <mouad3n@gmail.com>
2023-09-18 23:23:50 +0000

    Add 0x05 Pointers Arrays Strings Folder

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - This function do something.
+ *
+ * @s: Argument description.
+ *
+ * Return value description.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (lenght);
+}
