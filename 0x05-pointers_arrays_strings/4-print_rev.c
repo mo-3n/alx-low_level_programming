eb0c7c33603b2dd8a58608e50c3b772483707130
mo-3n <mouad3n@gmail.com>
2023-09-19 00:15:17 +0000

    Add 0x05 Pointers Arrays Strings Folder

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - This function do something.
+ *
+ * @str: Argument description.
+ *
+ * Return: value description.
+ */
+char _putchar(char s);
+
+void print_rev(char *s)
+{
+	int n = 0;
+	int b;
+
+	while (s[n] != '\0')
+	{
+		b = n;
+		n++;
+	}
+	while (s[b] >= 0)
+	{
+		_putchar(s[b]);
+		b--;
+	}
+	_putchar('\n');
+}
