ab1e8c3c79d006c885d66f4342ad491386398de4
mo-3n <mouad3n@gmail.com>
2023-09-22 00:04:11 +0000

    Add 0x06 Pointers Arrays Strings Folder

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - This function do something.
+ *
+ * @dest: Argument description.
+ * @src: Argument description.
+ *
+ * Return: return value description.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest[], src[], mix[];
+	
+	*mix = dest[] + src[];
+	*dest = mix[];
+	return (dest[]);
+}
