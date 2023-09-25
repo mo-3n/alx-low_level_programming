eaa2e000ebbc9421b03df5d2ec0594bc185e044d
mo-3n <mouad3n@gmail.com>
2023-09-25 00:40:36 +0000

    Add 0x06 Pointers Arrays Strings Folder

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,18 @@
  * _strcmp - This function do something.
  *
  * @s1: Argument description*
- * @s2: Argument description.
+ * @s2: Argument description*
  *
  * Return: return value description.
  */
 int *_strcmp(char *s1, char *s2)
 {
-	int valdiff;
-
-	while (*s1 != '\0' && *s1 == *s2)
+	while (*s1 != '\0')
 	{
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (*s1 - *s2);
 }
 
 
