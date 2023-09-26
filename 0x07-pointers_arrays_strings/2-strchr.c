dabd3c65ccbcd0fd3b9ab42220ba66326723ae25
mo-3n <mouad3n@gmail.com>
2023-09-26 23:02:09 +0000

    Add 0x07 Pointers Arrays Strings Folder

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s[i]);
 		}
-		return (NULL);
 	}
+	return (NULL);
 }
