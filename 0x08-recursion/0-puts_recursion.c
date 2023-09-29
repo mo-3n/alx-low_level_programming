4dd737bbf63dcae34cd4dae600afc837daa06ff0
mo-3n <mouad3n@gmail.com>
2023-09-29 20:04:13 +0000

    Add 0x08 Recursion Folder

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,6 +7,8 @@
  *
  * Return: return value description.
  */
+int _putchar(char c);
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(int s);
-
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
