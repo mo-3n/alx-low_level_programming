b5e19c7b26c90fc0aa0b05547cc3e401ddc94ef3
mo-3n <mouad3n@gmail.com>
2024-01-18 22:45:02 +0000

    Add 0x0C more malloc free Folder

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _memset - function description.
+ *
+ * @s: Argument description.
+ * @b: Argument description.
+ * @n: Argument description.
+ *
+ * Return: return value description.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ *
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - function description.
+ *
+ * @min: Argument description.
+ * @max: Argument description.
+ *
+ * Return: return value description.
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
+}
